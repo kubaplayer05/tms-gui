b74cff96e3ed4ab78f35ad31ae11f2056b4477c7
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2024-10-08 19:03:58 +0200

    feat: add information about delayed messages in backlogCard.tsx

--- src/components/dashboard/queue/backlogCard.tsx
+++ src/components/dashboard/queue/backlogCard.tsx
@@ -13,18 +13,22 @@ interface IBacklogCard {
 export default function BacklogCard({subscriptions}: IBacklogCard) {
 
     let msgCount: string | number = 0
+    let delayedMsgCount = 0
 
     for (const key in subscriptions) {
         msgCount += subscriptions[key].msgBacklog
+        delayedMsgCount += subscriptions[key].msgDelayed
     }
 
     msgCount = abbreviateNumber(msgCount)
 
     return (
         <DashboardPaperCard sx={{height: "100%", padding: "0.6rem 0"}}>
-            <Typography variant={"subtitle1"} align={"center"}>Backlog</Typography>
+            <Typography variant={"subtitle1"} align={"center"}>Backlog Messages</Typography>
             <Stack alignItems={"center"} justifyContent={"center"} sx={{height: "100%"}}>
                 <Typography sx={{paddingBottom: "2.4rem"}} variant={"h3"}>{msgCount}</Typography>
+                <Typography variant="subtitle1">with
+                    delayed: {delayedMsgCount}</Typography>
             </Stack>
         </DashboardPaperCard>
     )
